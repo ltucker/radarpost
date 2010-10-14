8290eec456a823c125c48428018385dd0616c3cb
Luke Tucker <ltucker@openplans.org>
2010-10-14 10:23:13 -0400

    add trim test

--- tests/test_mailbox.py
+++ tests/test_mailbox.py
@@ -26,3 +26,44 @@ def test_mailbox_count():
         count += r.value
     assert count == items_to_insert
 
+def test_mailbox_trim(): 
+    """
+    create a mailbox
+    add some dated messages 
+    trim out old messages
+    check correct messages remain
+    check old messages are not present
+    """
+    from datetime import datetime, timedelta
+    from radarpost.mailbox import Message, trim_mailbox
+    
+    # create a mailbox 
+    mb = create_test_mailbox()
+    
+    # insert 10 messages, each one day older 
+    # than the next, starting with today.
+    now = datetime.utcnow()
+    delta = timedelta(days=-1)
+    cur = now
+    messages = []
+    for i in range(10):
+        m = Message()
+        m.timestamp = cur
+        m.store(mb)
+        messages.append(m)
+        cur += delta
+        
+    # all messages should be in the mailbox now
+    for m in messages: 
+        assert m.id in mb
+    
+    # get rid of messages older than 5 days
+    trim_mailbox(mb, timedelta(days=5))
+    
+    for m in messages[:5]: 
+        assert m.id in mb
+    for m in messages[5:]:
+        assert not m.id in mb
+
+    
+    
