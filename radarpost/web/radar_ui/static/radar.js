169fb60f4a049d330b3058ee0ccc399024245da4
Luke Tucker <ltucker@openplans.org>
2010-11-16 12:44:01 -0500

    mostly visual improvments, reworking subscription ui

--- radarpost/web/radar_ui/static/radar.js
+++ radarpost/web/radar_ui/static/radar.js
@@ -5,3 +5,17 @@ var gettext = function(str) {
 var show_error = function(elid, message) {
     $('#' + elid + '_error').attr('innerHTML', message);
 };
+
+var mark_odd = function(selector, classname) {
+    $(selector).each(function(index, el) {
+       if (index % 2 == 1) {
+           $(el).addClass(classname);
+       }
+       else {
+           $(el).removeClass(classname);
+       }
+    });
+};
+
+
+
